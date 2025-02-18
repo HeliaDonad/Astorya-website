c496f644eae5352c316b39352f2b1e107b2794c2
Sedaezt <sedaezzat@icloud.com>
2025-02-18 21:47:27 +0100

    fix: carousel

--- js/image-carousel.js
+++ js/image-carousel.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const slide = document.querySelector(".carousel-slide");
-    const images = document.querySelectorAll(".carousel-slide img");
     const prevBtn = document.getElementById("prevBtn");
     const nextBtn = document.getElementById("nextBtn");
 
-    let counter = 0;
-    const imageWidth = images[0].clientWidth;
+    let index = 0;
 
-    nextBtn.addEventListener("click", () => {
-        if (counter >= images.length - 1) {
-            counter = -1;
+    nextBtn.addEventListener("click", function () {
+        if (index < 3) {
+            index++;
+        } else {
+            index = 0;
         }
-        counter++;
-        slide.style.transform = `translateX(${-imageWidth * counter}px)`;
+        updateCarousel();
     });
 
-    prevBtn.addEventListener("click", () => {
-        if (counter <= 0) {
-            counter = images.length;
+    prevBtn.addEventListener("click", function () {
+        if (index > 0) {
+            index--;
+        } else {
+            index = 3;
         }
-        counter--;
-        slide.style.transform = `translateX(${-imageWidth * counter}px)`;
+        updateCarousel();
     });
+
+    function updateCarousel() {
+        slide.style.transform = `translateX(-${index * 100}%)`;
+    }
 });
