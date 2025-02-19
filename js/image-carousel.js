0c7628ce5c33ea72d6c80ce0cbe80c6571431b26
HeliaDonad <r0936750@student.thomasmore.be>
2025-02-19 09:35:39 +0100

    feat: carousel update

--- js/image-carousel.js
+++ js/image-carousel.js
@@ -6,36 +6,34 @@ const nextBtn = document.getElementById('next-btn');
 const article1 = document.getElementById('article1');
 const article2 = document.getElementById('article2');
 const article3 = document.getElementById('article3');
+const article4 = document.getElementById('article4');
+
+const articles = [article1, article2, article3, article4];
 
 // Functie om naar het volgende artikel te navigeren
 function nextArticle() {
-    if (article1.classList.contains('active')) {
-        article1.classList.remove('active');
-        article2.classList.add('active');
-    } else if (article2.classList.contains('active')) {
-        article2.classList.remove('active');
-        article3.classList.add('active');
-    } else if (article3.classList.contains('active')) {
-        article3.classList.remove('active');
-        article1.classList.add('active');
+    for (let i = 0; i < articles.length; i++) {
+        if (articles[i].classList.contains('active')) {
+            articles[i].classList.remove('active');
+            const nextIndex = (i + 1) % articles.length; // Circulair naar het volgende artikel
+            articles[nextIndex].classList.add('active');
+            break;
+        }
     }
 }
 
 // Functie om naar het vorige artikel te navigeren
 function previewArticle() {
-    if (article1.classList.contains('active')) {
-        article1.classList.remove('active');
-        article3.classList.add('active');
-    } else if (article2.classList.contains('active')) {
-        article2.classList.remove('active');
-        article1.classList.add('active');
-    } else if (article3.classList.contains('active')) {
-        article3.classList.remove('active');
-        article2.classList.add('active');
+    for (let i = 0; i < articles.length; i++) {
+        if (articles[i].classList.contains('active')) {
+            articles[i].classList.remove('active');
+            const prevIndex = (i - 1 + articles.length) % articles.length; // Circulair naar het vorige artikel
+            articles[prevIndex].classList.add('active');
+            break;
+        }
     }
 }
 
 // Voeg event listeners toe aan de knoppen
 nextBtn.addEventListener('click', nextArticle);
 previewBtn.addEventListener('click', previewArticle);
-
