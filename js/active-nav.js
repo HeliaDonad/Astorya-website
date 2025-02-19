a35bf5b04f3e136b8523aa0f608dbc0e32f9b6df
HeliaDonad <r0936750@student.thomasmore.be>
2025-02-19 08:32:27 +0100

    fix: nav active

--- js/active-nav.js
+++ js/active-nav.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let currentPage = window.location.pathname.split("/").pop(); 
+    let currentPage = window.location.pathname.split("/").pop() || "index.html"; 
     let navLinks = document.querySelectorAll(".subnav a");
+
+    let isActiveSet = false;
 
     navLinks.forEach(link => {
         if (link.getAttribute("href").includes(currentPage)) {
             link.classList.add("active");
+            isActiveSet = true;
         }
     });
+
+    if (!isActiveSet) {
+        let homeLink = document.querySelector(".subnav a[href='index.html']");
+        if (homeLink) {
+            homeLink.classList.add("active");
+        }
+    }
 });
-
-
